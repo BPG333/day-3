37c1d39ceedb5a64f6bc0f78f842222648fb46ba
BPG333 <62209554+BPG333@users.noreply.github.com>
2020-09-04 14:53:23 +0300

    Add files via upload

---
+++ 2.5.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <script>
+        let name = "Pavel";
+
+        alert(`hello ${1}`); //display 'hello 1'
+
+        alert(`hello ${"name"}`); //display 'hello name'
+
+        alert(`hello ${name}`); //display 'hello Pavel'
+    </script>
+</body>
+
+</html>
