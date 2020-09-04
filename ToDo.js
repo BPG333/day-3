b900ea1e4107695c54539da84289fb288c56a32d
BPG333 <62209554+BPG333@users.noreply.github.com>
2020-09-04 14:21:08 +0300

    ToDo

---
+++ ToDo.js
@@ -0,0 +1,36 @@
+const todos = [];
+
+let globalId = 0;
+
+function getId() {
+  globalId++;
+  return globalId;
+}
+
+function addTodo(newTodo) {
+  todos.push({ id: getId(), title: newTodo, isComplete: false });
+}
+
+function removeTodo(todoId) {
+  const index = todos.findIndex((todo) => todo.id === todoId);
+  if (index === -1) {
+    throw new Error("Id not found");
+  }
+  todos.splice(index, 1);
+}
+
+function changeTodoTitle(todoId, newTitle) {
+  const index = todos.findIndex((todo) => todo.id === todoId);
+  if (index === -1) {
+    throw new Error("Id not found");
+  }
+  todos[index].title = newTitle;
+}
+
+function changeTodoStatus(todoId, isComplete) {
+  const index = todos.findIndex((todo) => todo.id === todoId);
+  if (index === -1) {
+    throw new Error("Id not found");
+  }
+  todos[index].isComplete = isComplete;
+}
